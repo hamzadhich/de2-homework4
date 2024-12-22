1a425cbbd7753a9adc09347fc9ab4d8e04e56a39
hamzadhich <hamzadhich@gmail.com>
2024-12-22 11:01:54 +0100

    Add files via upload

---
+++ bronze_views.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS hamza.bronze_views;
+CREATE EXTERNAL TABLE
+hamza.bronze_views (
+    title STRING,
+    views INT,
+    rank INT,
+    date DATE,
+    retrieved_at STRING
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://hamza-dhich-wikidata/datalake/bronze_views/';
